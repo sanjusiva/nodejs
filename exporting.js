39c9d6c27bcfb181e0f74211243182243b75ac68
sanjusiva <96572583+sanjusiva@users.noreply.github.com>
2022-02-22 22:58:38 +0530

    Importing variables and functions

---
+++ exporting.js
@@ -0,0 +1,7 @@
+module.exports={
+    name:"Sanjitha",
+    id:123,
+    speak:function(){
+        return "Hi";
+    }
+}
---
+++ importing.js
@@ -0,0 +1,4 @@
+const access=require("./exporting")
+console.log(access)//displays the whole content in that mentioned file path
+console.log(access.name)//displays the name(variable) value
+console.log(access.speak)//displays the functions value
